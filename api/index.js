bdb78e93c91e0ece6be94597918235e6acdfe846
Abdulquadri-Mahmud <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2023-11-30 16:48:23 +0100

    user models created succesfully

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
-
-app.listen(PORT, ()=> {
-    console.log(`Server is running on PORT ${PORT}`);
-})
+mongoose.connect(process.env.database).then(() => {
+    console.log('Database connected!');
+    app.listen(4000, () => {
+        console.log('Server is running on port 4000');
+    })
+}).catch((error) => {
+    console.log(error, 'Error while connecting to databse!');
+});
---
+++ api/models/user.models.js
@@ -0,0 +1,32 @@
+import mongoose  from "mongoose";
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    avatar : {
+        type: String,
+        default: "https://t4.ftcdn.net/jpg/05/09/59/75/360_F_509597532_RKUuYsERhODmkxkZd82pSHnFtDAtgbzJ.jpg"
+    }
+}, {timestamps : true});
+//Export the model
+const User = mongoose.model('User', userSchema);
+
+export default User;
